99c30e1de430f7a9ce4770f10acd7a056ab73e94
Manuel García-Llera Añón <manuelgarciallera@gmail.com>
2026-02-23 15:15:17 +0100

    Creación del componente blog

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { BlogComponent } from './components/blog/blog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BlogComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('actividad_5');
+
 }
