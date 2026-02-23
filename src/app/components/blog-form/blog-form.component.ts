43f095ffb5180d793eb871af35b6e28e4b10ee2b
Manuel García-Llera Añón <manuelgarciallera@gmail.com>
2026-02-23 15:42:57 +0100

    Generación de componentes blog, blog-form, blog-list e interface INoticia

---
+++ src/app/components/blog-form/blog-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-form',
+  imports: [],
+  templateUrl: './blog-form.component.html',
+  styleUrl: './blog-form.component.css',
+})
+export class BlogFormComponent {
+
+}
